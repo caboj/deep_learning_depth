fixed average pooling

src/deep_learning_depth.py
@@ -29,8 +29,10 @@ def main():
                         help='number of reblocks to use')
     parser.add_argument('-p', metavar="survival_p", dest="survival_p", type=float, 
                         help='set this to use stochastic resnet')
+    parser.add_argument('-f', metavar="filter_increase", dest="filt_inc", nargs='+', 
+                        help='specify at which resblocks the filters should double')
 
-    parser.set_defaults(epochs=100,samples=0,batch_size=1,depth=10,survival_p=0)
+    parser.set_defaults(epochs=100,samples=0,batch_size=20,depth=17,survival_p=.5,filt_inc=[3,7,13])
     args = parser.parse_args()
     
     epochs=args.epochs
@@ -39,6 +41,8 @@ def main():
     depth=args.depth
     samples_test = len(test_data[1]) if args.samples == 0 else args.samples
     pl=args.survival_p
+    filt_inc = args.filt_inc
+    print(filt_inc)
 
     X_train = np.array(X_train[:samples])
     y_train = np.array(y_train[:samples])
@@ -49,9 +53,9 @@ def main():
     #datagen.fit(X_train)
     #datagen.fit(X_test)
     
-    net = ResNet((3,32,32),depth,pl)
+    net = ResNet((3,32,32),depth,pl,filt_inc)
     resnet = net.get_net()
-    sgd =SGD(lr=0.0001, decay=1e-6,momentum=0.9)
+    sgd =SGD(lr=0.01, decay=1e-6,momentum=0.9)
     resnet.compile(optimizer=sgd,
               loss='categorical_crossentropy',
               metrics=['accuracy'])
@@ -60,9 +64,9 @@ def main():
     sample_z = SurvivalProb(depth,pl)
             
     resnet.fit(X_train,labels,batch_size=batch_size,nb_epoch=epochs,callbacks=[sample_z])
-#    resnet.fit_generator(datagen.flow(X_train,labels,batch_size=batch_size),samples_per_epoch=len(X_train),callbacks=[sample_z])
-
+    #    resnet.fit_generator(datagen.flow(X_train,labels,batch_size=batch_size),samples_per_epoch=len(X_train),callbacks=[sample_z])
     labels_test = to_categorical(y_test, 10)
+    resnet.z = np.ones(depth)
     score = resnet.evaluate(X_test, labels_test, batch_size=batch_size)
 
     print(score)

src/resnet.py
@@ -5,26 +5,31 @@ from numpy.random import binomial
 import numpy as np
 
 class ResNet(object):
-    def __init__(self,shape,depth,pl):
+    def __init__(self,shape,depth,pl,filt_inc):
 
         self.depth = depth
         self.pl = pl
         n = shape[1]*shape[2]
         inputs = Input(shape)
         self.z = np.arange(depth-1)#Input(shape=(depth-1,))
+        self.filter_increase = filt_inc
         self.filters = 64
         self.strides = 1
         
         inputs_n = Activation('relu')(BatchNormalization(mode=1)(Convolution2D(64,7,7,border_mode='same')(inputs)))#(Reshape(shape)(inputs))))
+        dim_decrease = 1
         for i in range(len(self.z)):
-            if np.mod(i+1,6) == 0:
+            if i in self.filter_increase:
+                dim_decrease += 1
                 self.strides = 2
                 self.filters = int(self.filters*2)
             else:
                 self.strides=1
             inputs_n = Activation('relu')(self.__res_block__(shape,n,inputs_n,self.z[i]))
 
-        out = AveragePooling2D(pool_size=(1,1))(inputs_n)
+
+        int(shape[2]/2**dim_decrease)
+        out = AveragePooling2D(pool_size=(int(shape[1]/2**dim_decrease),int(shape[2]/2**dim_decrease)))(inputs_n)
         out = Dense(10,activation='softmax')(Flatten()(out))
         self.resnet = Model(input=inputs,output=out)
 
@@ -59,5 +64,8 @@ class SurvivalProb(Callback):
         self.__resample_z()
 
     def __resample_z(self):
-        self.model.z = np.array([binomial(1,1-(d/self.depth)*(1-self.pl)) for d in range(1,self.depth)])
+        if self.pl == 1:
+            self.model.z = np.ones(depth)
+        else:
+            self.model.z = np.array([binomial(1,1-(d/self.depth)*(1-self.pl)) for d in range(1,self.depth)])
     